cde25eb728cf9d0bda584929180d60ddb658e6c4
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-07-06 08:35:18 +1000

    COmmandline runner bean

--- runnerz/runnerz/src/main/java/com/example/runnerz/RunnerzApplication.java
+++ runnerz/runnerz/src/main/java/com/example/runnerz/RunnerzApplication.java
@@ -1,10 +1,18 @@
 package com.example.runnerz;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.example.runnerz.run.Location;
+import com.example.runnerz.run.Run;
 
 
 
@@ -15,9 +23,17 @@ public class RunnerzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
-		log.info("Application has started successfully");
+		
 
 
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args ->{
+			Run run = new Run(1,"First run", LocalDateTime.now(),LocalDateTime.now().plus(1,ChronoUnit.HOURS),5,Location.OUTDOOR);
+			log.info("Run: "+run);
+		};
 	}
 
 }
