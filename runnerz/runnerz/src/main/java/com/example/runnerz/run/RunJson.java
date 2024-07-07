029628792a9238af3d3f795acbde9862155cc3ab
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-07-07 14:22:43 +1000

    stuck at 2:00 docker postgresql

--- runnerz/runnerz/src/main/java/com/example/runnerz/run/RunJson.java
+++
@@ -1,3 +0,0 @@
-package com.example.runnerz;
-
-public class RunJsonDataLoader 
--- runnerz/runnerz/src/main/java/com/example/runnerz/run/RunJsonDataLoader.java
+++ runnerz/runnerz/src/main/java/com/example/runnerz/run/RunJsonDataLoader.java
@@ -7,11 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aot.hint.TypeReference;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import com.example.runnerz.RunnerzApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+@Component
 public class RunJsonDataLoader implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
@@ -28,7 +29,7 @@ public class RunJsonDataLoader implements CommandLineRunner {
         if(runRepository.count()==0){
             try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/runs.json")){
                 Runs allRuns = objectMapper.readValue(inputStream,Runs.class);
-                log.info("Reading {} runs from JSON data and saving to in-memory collection", allRuns.runs().size());
+                log.info("Reading {} runs from JSON data and saving to a database", allRuns.runs().size());
                 runRepository.saveAll(allRuns.runs());
             } catch (IOException e){
                 throw new RuntimeException("Failed to read JSON data",e);
